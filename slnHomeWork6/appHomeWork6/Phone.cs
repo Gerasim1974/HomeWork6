ef1719f5550927776dc9ffecfd24453deaf1cead
ПК <GIS_1974@tut.by>
2022-07-29 20:23:17 +0300

    HomeWork6

---
+++ slnHomeWork6/appHomeWork6/Phone.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appHomeWork6
+{
+    public class Phone
+    {
+       private  string _number;
+       private string _model;
+       private double _wheight;
+
+       public Phone(string number, string model, double wheight)
+            {
+            Number = number;
+            Model = model;
+            Wheight = wheight;
+            }
+        public string Number
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            { 
+                _number = value;
+            }
+        }
+        public string Model
+        {
+            get 
+            { 
+                return _model; 
+            } 
+            private set
+            {
+                _model = value;
+            }
+        }
+        public double Wheight
+        {
+            private set 
+            {
+                if (value < 0)
+                {
+                    _wheight = 0;
+                }
+                else
+                {
+                    _wheight = value;
+                }
+            }
+            get
+            {
+                return _wheight;
+            }
+        }
+        public void ReciveCall(string nameCall)
+        {
+            Console.WriteLine($"Вам звонил {nameCall}");
+        }
+    }
+}
---
+++ slnHomeWork6/appHomeWork6/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace appHomeWork6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sNumberPhone;
+            string sModelPhone;
+            double dWeightPhone;
+
+            do
+            {
+
+                Console.WriteLine("Введите номер телефона");
+                sNumberPhone = Console.ReadLine();
+                Console.WriteLine("введите модель телефона");
+                sModelPhone = Console.ReadLine();
+                Console.WriteLine("Введите вес телефона");
+                dWeightPhone = double.Parse(Console.ReadLine());
+                Phone myPhone = new(sNumberPhone, sModelPhone, dWeightPhone);
+
+                Console.WriteLine($"Ваш номер телефона {myPhone.Number}");
+                Console.WriteLine($"Модель Вашего телефона {myPhone.Model}");
+                Console.WriteLine($"Вес Вашего телефона {myPhone.Wheight}");
+
+                Console.WriteLine("Введите имя");
+                string sNameMan = Console.ReadLine();
+                myPhone.ReciveCall(sNameMan);
+                Console.WriteLine("Для выхода из программ нажмите ESC, для продолжения нажмите любую кнопку");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+        }
+    }
+}
